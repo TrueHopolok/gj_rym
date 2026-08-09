cf8da268116961992af57f3c8594f6870239956c
TrueHopolok <vadim.truhanov@inbox.lv>
2026-08-09 19:43:05 +0300

    yooo

--- scenes/exit/exit.gd
+++ scenes/exit/exit.gd
@@ -5,6 +5,7 @@ extends Area2D
 @onready var _door: Sprite2D = %Door
 @onready var _light: Sprite2D = %Light
 @onready var _exit_position: Marker2D = %ExitPosition
+@onready var _sfx_door: AudioStreamPlayer = $SFXDoor
 
 
 func _ready() -> void:
@@ -36,14 +37,16 @@ func _do_exit_sequence(player: Player) -> void:
 	print("exit seq")
 	player.state = Player.PLAYER_STATES.BUSY
 
+	_sfx_door.play()
 	var t: Tween = create_tween().chain()
 	t.tween_property(player, "global_position", _exit_position.global_position, 0.5)
-	t.tween_property(_door, "position", Vector2.UP * 64, 1).as_relative().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	t.tween_property(_door, "position", Vector2.UP * 64, 0.9).as_relative().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
 	t.parallel().tween_property(_light, "modulate:a", 1.0, 1)
-	t.chain().tween_callback(func () -> void:
+	t.chain().tween_callback(func() -> void:
 		player.z_index -= 1
 	)
 	t.chain().tween_interval(1.0)
-	t.chain().tween_property(_door, "position", Vector2.DOWN * 64, 2).as_relative().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
+	t.chain().tween_callback(_sfx_door.play)
+	t.chain().tween_property(_door, "position", Vector2.DOWN * 64, 0.9).as_relative().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 
 	t.tween_callback(_exited)
