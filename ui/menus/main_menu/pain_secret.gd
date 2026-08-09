f20c89dd36026741eb8436b85af5075ea714dd4a
TrueHopolok <vadim.truhanov@inbox.lv>
2026-08-09 21:09:22 +0300

    added secret path of pain

---
+++ ui/menus/main_menu/pain_secret.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+var _cur: Array[int] = [0, 0, 0, 0]
+var _goal: Array[int] = [KEY_P, KEY_A, KEY_I, KEY_N]
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not event is InputEventKey:
+		return
+	var key_event: InputEventKey = event as InputEventKey
+	if not key_event.pressed or key_event.echo:
+		return
+	_cur.pop_front()
+	_cur.push_back(key_event.keycode)
+	if _cur == _goal:
+		Transition.change_scene_path("res://scenes/levels/path_of_pain.tscn")
