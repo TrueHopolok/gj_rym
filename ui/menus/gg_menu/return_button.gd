f8ffd2d3722c3817a241258d2ab4803c02ce2d82
TrueHopolok <vadim.truhanov@inbox.lv>
2026-08-09 15:38:51 +0300

    gg menu done and contrinue btn in main menu

---
+++ ui/menus/gg_menu/return_button.gd
@@ -0,0 +1,5 @@
+extends BetterButton
+
+
+func _on_press() -> void:
+	Transition.change_scene_path("res://ui/menus/main_menu/main_menu.tscn")
--- ui/menus/main_menu/continue_button.gd
+++ ui/menus/main_menu/continue_button.gd
@@ -3,12 +3,12 @@ extends BetterButton
 
 const CAMPAIGN_PATH: String = "res://scenes/levels/campaign/campaign%d.tscn"
 
-@export_range(1, 10, 1) var _lvl_num: int = 1
+@onready var _lvl_num: int = clampi(Persistence.unlocked_level, 1, 10)
 
 
 func _ready() -> void:
 	super()
-	disabled = _lvl_num > Persistence.unlocked_level
+	disabled = _lvl_num <= 1
 
 
 func _on_press() -> void:
