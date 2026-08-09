f199859102cba2607135c4eb46c71f1c2f347b15
TrueHopolok <vadim.truhanov@inbox.lv>
2026-08-09 23:36:44 +0300

    added ui btn sfx

--- ui/better_button/better_button.gd
+++ ui/better_button/better_button.gd
@@ -2,7 +2,8 @@ class_name BetterButton
 extends Button
 
 # use load("sfx") to set some sfx as default
-@export var _press_sfx: AudioStream
+@export var _press_sfx: AudioStream = load("res://assets/audio/sfx/ui_button.mp3")
+@export var _volume_db: int = -36
 
 
 func _ready() -> void:
@@ -17,6 +18,7 @@ func _on_press() -> void:
 
 func _set_sfx() -> void:
 	var press_sfx_player: AudioStreamPlayer = AudioStreamPlayer.new()
+	press_sfx_player.volume_db = _volume_db
 	press_sfx_player.stream = _press_sfx
 	pressed.connect(press_sfx_player.play)
 	add_child(press_sfx_player)
