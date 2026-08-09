cd22d09163917370bf286c50f9f3bfc16d8aeb95
TrueHopolok <vadim.truhanov@inbox.lv>
2026-08-09 19:58:02 +0300

    aaa

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,8 +5,8 @@ signal died
 signal exited
 
 enum PLAYER_STATES {
-	SKIP = 10,  # used for cutscenes / manual control
-	BUSY = 20,  # same but has gravitation
+	SKIP = 10, # used for cutscenes / manual control
+	BUSY = 20, # same but has gravitation
 	IDLE = 30,
 	MOVE = 40,
 	JUMP = 50,
@@ -25,7 +25,7 @@ const SpikeType = CorpseSpiked.SpikeType
 const KICK_FORCE: Vector2 = Vector2(200, -250)
 const RUN_FORCE: float = 900.0
 const JUMP_FORCE: float = 400.0
-const POGO_FORCE: float = 450.0
+const POGO_FORCE: float = 500.0 # tmp solution, pls find what eats regular pogo
 const MEGA_FORCE: float = 500.0
 const MAX_RUNNING_SPEED: float = 250.0
 const MAX_FALLING_SPEED: float = 500.0
@@ -167,7 +167,7 @@ func _movement_pogo() -> void:
 			_sfx_mega.play()
 		else:
 			_buffer_mega.start(BUFFER_MEGA_LENGTH)
-		velocity.y = -force
+		velocity.y = - force
 		corpse.apply_central_impulse(Vector2.DOWN * 100)
 		add_collision_exception_with(corpse)
 		get_tree().create_timer(0.2).timeout.connect(remove_collision_exception_with.bind(corpse))
@@ -181,7 +181,7 @@ func _movement_jump() -> void:
 		_buffer_jump.stop()
 		_buffer_mega.stop()
 		_has_cancel = true
-		velocity.y = -MEGA_FORCE
+		velocity.y = - MEGA_FORCE
 		_sfx_mega.play()
 		return
 	if !is_on_floor() && _buffer_coyote.is_stopped():
@@ -190,7 +190,7 @@ func _movement_jump() -> void:
 	_buffer_coyote.stop()
 
 	_has_cancel = true
-	velocity.y = -JUMP_FORCE
+	velocity.y = - JUMP_FORCE
 	_sfx_jumping.play()
 
 
@@ -208,7 +208,7 @@ func _update_animations() -> void:
 		_flipper.scale.x = signf(velocity.x)
 
 	if _sprite.animation == &"kick":
-		return  # let it play out
+		return # let it play out
 
 	match state:
 		PLAYER_STATES.IDLE, PLAYER_STATES.BUSY, PLAYER_STATES.SKIP:
